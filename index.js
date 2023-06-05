bbfc98f082f2fe505548087db57a9ffa2292988b
Sherry Wang <y2683wan@uwaterloo.ca>
2023-06-05 19:34:11 +1000

    add fcpx convert func

--- index.js
+++ index.js
@@ -10,17 +10,50 @@
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
-const generate_subtitles = require('./utils/generate_subtitles.js')
+const fcpxml = require('./utils/fcpxml')
 const input = cli.input;
-const flags = cli.flags;
-const { clear } = flags;
+// const flags = cli.flags;
+// const { clear } = flags;
+
+// const process = require('process')
 
 (async () => {
-	init({ clear });
-	// input.includes(`help`) && cli.showHelp(0);
+	// init({ clear });
+	let [srt_path, fps, destination_path] = cli.input
 
-	flags.name && flags.software && flags.destination && (
-		await generate_subtitles(flags.name, flags.software, flags.destination)
+	if (!srt_path) {
+		console.error('Specify a SRT file')
+		process.exit(1)
+	}
 
-	)
+	if (srt_path && !srt_path.endsWith('.srt')) {
+		console.error('Enter correct SRT file')
+		process.exit(1)
+	}
+
+	if (!fps) {
+		console.error('Specify frame rate ')
+		process.exit(1)
+	}
+
+	if (fps && !parseFloat(fps)) {
+		console.error('Enter correct fps number')
+		process.ext(1)
+	}
+
+	if (srt_path && !srt_path.endsWith('.srt') && fps && !parseFloat(fps)) {
+		console.error('Enter correct SRT file')
+		console.error('Enter correct fps number')
+		process.exit(1)
+	}
+
+	if (!destination_path) {
+		destination_path = process.cwd()
+	}
+
+	
+	console.log(srt_path, fps, destination_path)
+
+	fcpxml(srt_path, fps, destination_path)
+
 })();
