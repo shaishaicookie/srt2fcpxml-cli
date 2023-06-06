79f304f92c5e26cfcf7d1feb58a3a002c67cf7af
Sherry Wang <y2683wan@uwaterloo.ca>
2023-06-06 20:58:35 +1000

    Delete generate_subtitles.js

--- utils/generate_subtitles.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-
-
-module.exports = async function generate_subtitles(name, software, destination) {
-    console.log(name, software, destination)
-}
